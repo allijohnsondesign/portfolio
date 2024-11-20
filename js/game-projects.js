f98266659074aa0786e99f95bba8beab9d0b67a3
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-11-19 21:29:54 -0500

    added the rest of the files

--- js/game-projects.js
+++ js/game-projects.js
@@ -79,13 +79,61 @@ let carousel1Elements = {
         'Options Menu Final | Godot'
     ],
     bodies: [
-        'Focused on 2 interactions: the options menu and the character customization menu',
+        'Focused on 2 interactions: the options menu and the character customization menu.',
         'In the character customization menu, the user can choose the pronouns, voice, and color palette of their character. (Visuals by Kylee Dicken)',
         'In the options menu, the user can adjust the volume settings of the voices and music, as well as the text speed. (Visuals by Kylee Dicken)'
     ]
     
 }
 
+let carousel2Elements = {
+    background: document.getElementById("carousel-bg-2"),
+    leftArrow: document.getElementById("carousel-left-2"),
+    rightArrow: document.getElementById("carousel-right-2"),
+    contentElement: document.getElementById("carousel-content-2"),
+    headerElement: document.getElementById("process-header-2"),
+    bodyElement: document.getElementById("process-body-2"),
+    content: [
+        '<video src="assets/images/Archer in Training Process/TeamHyperpopMilestone.mp4" controls></video>',
+        '<video src="assets/images/Archer in Training Process/Hyperpop Archer_In_Training_FINAL.mp4" controls></video>'
+    ],
+    headers: [
+        'Prototype | Godot',
+        'Final Gameplay | Godot'
+    ],
+    bodies: [
+        'Experimenting with UX implementation in project 1, and testing 3D movement in project 2, though this was scrapped.',
+        'Now complete with visuals, narrative, sound design, and UI/UX design, “Archer in Training” contains 3 levels that get more difficult as the player progresses.'
+    ]
+    
+}
+
+let carousel3Elements = {
+    background: document.getElementById("carousel-bg-3"),
+    leftArrow: document.getElementById("carousel-left-3"),
+    rightArrow: document.getElementById("carousel-right-3"),
+    contentElement: document.getElementById("carousel-content-3"),
+    headerElement: document.getElementById("process-header-3"),
+    bodyElement: document.getElementById("process-body-3"),
+    content: [
+        '<video src="assets/images/Archer in Training Process/TeamHyperpopMilestone.mp4" controls></video>',
+        '<video src="assets/images/Archer in Training Process/Hyperpop Archer_In_Training_FINAL.mp4" controls></video>'
+    ],
+    headers: [
+        'Prototype | Figma',
+        'Low Fidelity Mockup | Figma',
+        'High Fidelity Final | Figma'
+    ],
+    bodies: [
+        'I created a low-fidelity mockup in Figma based on a blueprint created by the Lead Interactive Designer, Brad McKinney.',
+        'During my internship, I designed the low fidelity mockup for the interactive. This included experimenting with the user flow, creating themed assets, and interpreting the original blueprint provided. (No video available)',
+        'Based on the low fidelity mockup from my internship, Brad McKinney finalized the design of the interactive, complete with animated transitions and the final visual design.'
+    ]
+    
+}
+
 carousel1 = new Carousel([], carousel1Elements);
+carousel2 = new Carousel([], carousel2Elements);
 
 carousel1.click_listeners()
+carousel2.click_listeners()
